3f95623f12010a100907c742366a466eec159abd
Adam Jensen <acjensen@gmail.com>
2017-12-26 17:54:45 -0500

    Add password to the configuration structs

--- lib/faktory/configuration/worker.ex
+++ lib/faktory/configuration/worker.ex
@@ -3,7 +3,7 @@ defmodule Faktory.Configuration.Worker do
 
   defstruct [
     host: "localhost", port: 7419, pool: nil, middleware: [], fn: nil,
-    name: "default", concurrency: 20, wid: nil, queues: ["default"]
+    name: "default", concurrency: 20, wid: nil, queues: ["default"], password: nil
   ]
 
 end
