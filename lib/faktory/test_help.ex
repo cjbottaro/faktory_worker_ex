1648ba3376d90e0ffd89c88478fe7a8d26548340
Christopher J. Bottaro <cjbottaro@alumni.cs.utexas.edu>
2017-12-28 18:17:40 -0600

    Remove unnecessary else

--- lib/faktory/test_help.ex
+++ lib/faktory/test_help.ex
@@ -2,12 +2,7 @@ defmodule Faktory.TestHelp do
 
   defmacro if_test(do: block) do
     if Faktory.Utils.env == :test do
-      quote do
-        unquote(block)
-      end
-    else
-      quote do
-      end
+      quote do: unquote(block)
     end
   end
 
