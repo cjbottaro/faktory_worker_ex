e66ccc53ab5962e796c4c6cc7c2b5f3262f047df
Christopher J. Bottaro <cjb@cjbotta.ro>
2020-06-02 12:20:48 -0500

    Error tuple

--- lib/faktory.ex
+++ lib/faktory.ex
@@ -82,7 +82,7 @@ defmodule Faktory do
         Logger.info "Q 🕒 #{inspect self()} jid-#{jid} (#{jobtype}) #{inspect(args)}"
         {:ok, job}
 
-      error -> error
+      {:error, reason} -> {:error, reason}
     end
   end
 
